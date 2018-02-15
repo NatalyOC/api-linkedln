e2a5dd3b55f24d81728b777bdf83f984d7423e6a
NatalyOC <noteroc@hotmail.com>
2018-02-15 04:29:07 -0500

    agregando funcionalidad a nueva vista

--- public/js/app.js
+++ public/js/app.js
@@ -3,40 +3,31 @@ function OnLinkedInFrameworkLoad() {
   IN.Event.on(IN, 'auth', OnLinkedInAuth);
 }
 // Función que permite usar la llamada para solicitar los datos del perfil del miembro
-function OnLinkedInAuth() {
-  IN.API.Profile('me').result(ShowProfileData);
+function OnLinkedInAuth(e) {
+  IN.API.Profile('me').fields('id', 'first-name', 'last-name', 'headline', 'location', 'picture-url', 'public-profile-url', 'email-address').result(ShowProfileData).error(onError);
+  // e.preventDefault();
+  window.location.href = '../views/profile.html';
 }
-// Funcion que permite mostrar información de perfil de miembro en la página web.
-function ShowProfileData(profiles) {
-  var member = profiles.values[0];
-  var id = member.id;
-  var firstName = member.firstName; 
-  var lastName = member.lastName; 
-  var photo = member.pictureUrl; 
-  var headline = member.headline; 
-
-  // use information captured above
-}
- 
-/*  */
-// Setup an event listener to make an API call once auth is complete
-/* function onLinkedInLoad() {
-  IN.Event.on(IN, 'auth', getProfileData);
-}
-
-// Handle the successful return from the API call
-function onSuccess(data) {
-  console.log(data);
-}
-
-// Handle an error response from the API call
 function onError(error) {
   console.log(error);
 }
-
-// Use the API call wrapper to request the member's basic profile data
-function getProfileData() {
-  IN.API.Raw('/people/~').result(onSuccess).error(onError);
+// Funcion que permite mostrar información de perfil de miembro en la página web.
+function ShowProfileData(profiles) {
+  let member = profiles.values[0];
+  let id = member.id;
+  let firstName = member.firstName; 
+  let lastName = member.lastName; 
+  let photo = member.pictureUrl; 
+  let headline = member.headline; 
+  let locationName = member.location.name;
+  let publicProfileUrl = member.publicProfileUrl;
+  // Persistencia de Data
+  localStorage.firstName = firstName;
+  localStorage.lastName = lastName;
+  localStorage.photo = photo;
+  localStorage.locationName = locationName;
+  localStorage.headline = headline;
+  localStorage.publicProfileUrl = publicProfileUrl;
+  console.log(id, locationName); 
 }
-
-alert('hi'); */
+ 
