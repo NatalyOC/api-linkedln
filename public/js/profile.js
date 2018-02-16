64712b001e6b56aaf6a04dfc90ff96293263181b
NatalyOC <noteroc@hotmail.com>
2018-02-16 01:39:13 -0500

    modificando según codereview

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> f5426737fae2cc523b229223bfb791e86305d18f
+
 // Toggle profile description
 $('.card-profile-stats-more-link').click(function(event) {
   event.preventDefault();
@@ -12,37 +9,11 @@ $('.card-profile-stats-more-link').click(function(event) {
   }
   $(this).next('.card-profile-stats-more-content').slideToggle();
 });
-<<<<<<< HEAD
-=======
-// use information captured above
-// alert('hi');
-// document.getElementById('picture').innerHTML = '<img src="' + localStorage.photo + '" />';
-document.getElementById('pic').setAttribute('src', localStorage.profilePhoto);
-document.getElementById('name').innerHTML = localStorage.fname + ' ' + localStorage.lname;
-document.getElementById('intro').innerHTML = localStorage.headline;
-document.getElementById('email').innerHTML = localStorage.email;
-document.getElementById('location').innerHTML = localStorage.locationName ;
-document.getElementById('link').innerHTML = '<a href="' + localStorage.publicProfileUrl + '" target="_blank">Visit profile</a>';
-=======
->>>>>>> f5426737fae2cc523b229223bfb791e86305d18f
-// use information captured above
-// alert('hi');
-// document.getElementById('picture').innerHTML = '<img src="' + localStorage.photo + '" />';
-document.getElementById('pic').setAttribute('src', localStorage.profilePhoto);
-document.getElementById('name').innerHTML = localStorage.fname + ' ' + localStorage.lname;
-document.getElementById('intro').innerHTML = localStorage.headline;
-document.getElementById('email').innerHTML = localStorage.email;
-document.getElementById('location').innerHTML = localStorage.locationName ;
-document.getElementById('link').innerHTML = '<a href="' + localStorage.publicProfileUrl + '" target="_blank">Visit profile</a>';
-<<<<<<< HEAD
-=======
-document.getElementById('profileData').style.display = 'block';
-/* 
-function logout() {
-  IN.User.logout();
-}
 
-document.getElementById('button-logout').addEventListener('click',logout);
- */
->>>>>>> 33bfef65b46a837ba3795c9a09e12e566c4413d3
->>>>>>> f5426737fae2cc523b229223bfb791e86305d18f
+document.getElementById('pic').setAttribute('src', localStorage.profilePhoto);
+document.getElementById('name').innerHTML = localStorage.fname + ' ' + localStorage.lname;
+document.getElementById('intro').innerHTML = localStorage.headline;
+document.getElementById('email').innerHTML = localStorage.email;
+document.getElementById('location').innerHTML = localStorage.locationName ;
+document.getElementById('link').innerHTML = '<a href="' + localStorage.publicProfileUrl + '" target="_blank">Visit profile</a>';
+
