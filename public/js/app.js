33a1b87e93683224f76ad6d43f7800000ce9b2fc
NatalyOC <noteroc@hotmail.com>
2018-02-16 00:13:18 -0500

    modificando según codereview

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,41 @@
-// Funcion que detecta el evento para realizar una llamada a la API una vez se complete la autenticación
-function OnLinkedInFrameworkLoad() {
-  IN.Event.on(IN, 'auth', OnLinkedInAuth);
-}
-// Función que permite usar la llamada para solicitar los datos del perfil del miembro
-function OnLinkedInAuth(e) {
-  IN.API.Profile('me').fields('id', 'first-name', 'last-name', 'headline', 'location', 'picture-url', 'public-profile-url', 'email-address').result(ShowProfileData).error(onError);
-  // e.preventDefault();
-  window.location.href = '../views/profile.html';
-}
-function onError(error) {
-  console.log(error);
-}
-// Funcion que permite mostrar información de perfil de miembro en la página web.
-function ShowProfileData(profiles) {
-  let member = profiles.values[0];
-  let id = member.id;
-  let firstName = member.firstName; 
-  let lastName = member.lastName; 
-  let photo = member.pictureUrl; 
-  let headline = member.headline; 
-  let locationName = member.location.name;
-  let publicProfileUrl = member.publicProfileUrl;
-  // Persistencia de Data
-  localStorage.firstName = firstName;
-  localStorage.lastName = lastName;
-  localStorage.photo = photo;
-  localStorage.locationName = locationName;
-  localStorage.headline = headline;
-  localStorage.publicProfileUrl = publicProfileUrl;
-  console.log(id, locationName); 
+// Setup an event listener to make an API call once auth is complete
+function onLinkedInLoad() {
+  IN.Event.on(IN, 'auth', getProfileData);
 }
  
+// Logout user
+function logout() {
+  IN.User.logout(onLogout);
+}
+ 
+function onLogout() {
+  console.log('Logout successfully');
+  window.location.href = 'views/profile.html';
+}
+ 
+// Use the API call wrapper to request the member's basic profile data
+function getProfileData() {
+  IN.API.Profile('me').fields('id', 'first-name', 'last-name', 'headline', 'location', 'picture-url', 'public-profile-url', 'email-address').result(function(data) {
+    console.log(data);
+    let userdata = data.values[0];
+    let id = userdata.id;
+    let fname = userdata.firstName;
+    let lname = userdata.lastName;
+    let email = userdata.emailAddress;
+    let profilePhoto = userdata.pictureUrl;
+    let headline = userdata.headline; 
+    let locationName = userdata.location.name;
+    let publicProfileUrl = userdata.publicProfileUrl;
+    logout();
+    localStorage.id = id;
+    localStorage.fname = fname;
+    localStorage.lname = lname;
+    localStorage.email = email;
+    localStorage.profilePhoto = profilePhoto;
+    localStorage.locationName = locationName;
+    localStorage.headline = headline;
+    localStorage.publicProfileUrl = publicProfileUrl;
+  }).error(function(data) {
+    console.log(data);
+  });
+}
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,9 +1,19 @@
- // use information captured above
- alert('hi');
- //document.getElementById('picture').innerHTML = '<img src="' + localStorage.photo + '" />';
- document.getElementById('name').innerHTML = localStorage.firstName + ' ' + localStorage.lastName;
- document.getElementById('intro').innerHTML = localStorage.headline;
- document.getElementById('email').innerHTML = localStorage.emailAddress;
- document.getElementById('location').innerHTML = localStorage.locationName;
- document.getElementById('link').innerHTML = '<a href="' + localStorage.publicProfileUrl + '" target="_blank">Visit profile</a>';
- document.getElementById('profileData').style.display = 'block';
+// Toggle profile description
+$('.card-profile-stats-more-link').click(function(event) {
+  event.preventDefault();
+  if ($('.card-profile-stats-more-content').is(':hidden')) {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-down').addClass('fa-angle-up');
+  } else {
+    $('.card-profile-stats-more-link').find('i').removeClass('fa-angle-up').addClass('fa-angle-down');
+  }
+  $(this).next('.card-profile-stats-more-content').slideToggle();
+});
+// use information captured above
+// alert('hi');
+// document.getElementById('picture').innerHTML = '<img src="' + localStorage.photo + '" />';
+document.getElementById('pic').setAttribute('src', localStorage.profilePhoto);
+document.getElementById('name').innerHTML = localStorage.fname + ' ' + localStorage.lname;
+document.getElementById('intro').innerHTML = localStorage.headline;
+document.getElementById('email').innerHTML = localStorage.email;
+document.getElementById('location').innerHTML = localStorage.locationName ;
+document.getElementById('link').innerHTML = '<a href="' + localStorage.publicProfileUrl + '" target="_blank">Visit profile</a>';
